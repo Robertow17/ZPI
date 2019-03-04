1ffd1368d5aa5de89a22ddc4ba0831b2ba053f1d
Patryk <patryk.ostaszewski@gmail.com>
2019-03-04 22:43:56 +0100

    Refaktoryzacja
    Refaktoryzacja nazwy pakietow z com.demo.asus.zpi na com.zpi

---
+++ app/src/main/java/com/zpi/budget/BudgetActivity.java
@@ -0,0 +1,15 @@
+package com.zpi.budget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zpi.R;
+
+public class BudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.budget_activity);
+    }
+}
