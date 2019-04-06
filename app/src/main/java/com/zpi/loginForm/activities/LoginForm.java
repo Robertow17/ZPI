5a5786dad88b1128d376980cf64c050a175fe767
Wojciech Kopacki <kopackiwojciech@gmail.com>
2019-04-06 15:17:39 +0200

    chore: add sign up/in click listeners

--- app/src/main/java/com/zpi/loginForm/activities/LoginForm.java
+++ app/src/main/java/com/zpi/loginForm/activities/LoginForm.java
@@ -2,6 +2,9 @@ package com.zpi.loginForm.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.zpi.R;
 
@@ -11,5 +14,29 @@ public class LoginForm extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_form);
+        setSignInButtonListener();
+        setSignUpButtonListener();
+    }
+
+    private void setSignInButtonListener() {
+        Button signUpButton = findViewById(R.id.signInButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Sign in listener active", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    private void setSignUpButtonListener() {
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Sign up listener active", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
