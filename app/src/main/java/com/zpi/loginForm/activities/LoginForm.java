b44f6570c0f2f49a390fe1b1406bc175515c0f38
Wojciech Kopacki <kopackiwojciech@gmail.com>
2019-04-06 15:22:08 +0200

    chore: add password field extraction

--- app/src/main/java/com/zpi/loginForm/activities/LoginForm.java
+++ app/src/main/java/com/zpi/loginForm/activities/LoginForm.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zpi.R;
@@ -18,12 +19,20 @@ public class LoginForm extends AppCompatActivity {
         setSignUpButtonListener();
     }
 
+    private String getPassword() {
+        TextView passwordInput = findViewById(R.id.passwordText);
+        String password = passwordInput.getText().toString();
+
+        return password;
+    }
+
     private void setSignInButtonListener() {
         Button signUpButton = findViewById(R.id.signInButton);
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Sign in listener active", Toast.LENGTH_SHORT);
+                String password = getPassword();
+                Toast toast = Toast.makeText(getApplicationContext(), "Password: " + password, Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
@@ -34,7 +43,8 @@ public class LoginForm extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Sign up listener active", Toast.LENGTH_SHORT);
+                String password = getPassword();
+                Toast toast = Toast.makeText(getApplicationContext(), "Password: " + password, Toast.LENGTH_SHORT);
                 toast.show();
             }
         });
