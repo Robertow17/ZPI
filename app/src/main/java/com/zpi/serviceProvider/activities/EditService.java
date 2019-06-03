01386d7580838ae00e621fc04a12bc9653708c3b
Wojciech Kopacki <kopackiwojciech@gmail.com>
2019-06-03 17:10:51 +0200

    chore: whatever

--- app/src/main/java/com/zpi/serviceProvider/activities/EditService.java
+++ app/src/main/java/com/zpi/serviceProvider/activities/EditService.java
@@ -20,7 +20,6 @@ import com.zpi.R;
 import com.zpi.ServerConnector.ServerConnector;
 import com.zpi.ServerConnector.ServiceName;
 import com.zpi.model.Category;
-import com.zpi.model.Photo;
 import com.zpi.model.Service;
 import com.zpi.model.Subcategory;
 import com.zpi.model.TransportDetails;
@@ -28,36 +27,25 @@ import com.zpi.model.WeddingHallDetails;
 import com.zpi.searcher.utils.PageTransformer;
 import com.zpi.searcher.utils.ServicesAdapter;
 import com.zpi.searcher.utils.ViewPagerAdapter;
-import com.zpi.serviceProvider.model.ServiceProvider;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static com.zpi.searcher.utils.ItemViewPagerAdapter.EXTRA_POSITION;
+import java.util.stream.Collectors;
 
 public class EditService extends AppCompatActivity {
-    private static final int PICK_IMAGE_REQUEST = 1;
-
     private EditText name, localization, description, email, phone, maxsize;
     private TextView maxsizeText;
     private TextView accommodationText;
     private CheckBox accommodation;
     private Spinner category, subcategory;
 
-
-    private Photo nophotos;
     private ViewPagerAdapter viewPagerAdapter;
 
     List<Category> categories;
     List<Subcategory> subcategories;
     Service service;
-    ServiceProvider currentServiceProvider;
-
-    int pos;
-    int index;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,9 +53,6 @@ public class EditService extends AppCompatActivity {
         setContentView(R.layout.service_provider_add_service_activity);
 
         service = getIntent().getParcelableExtra(ServicesAdapter.EXTRA_SERVICE);
-        pos = Objects.requireNonNull(getIntent().getExtras()).getInt(EXTRA_POSITION);
-        index = service.getId();
-        currentServiceProvider = com.zpi.serviceProvider.model.Data.getServiceProvider();
 
         Data.setSubcategories();
         categories = Data.getCategories();
@@ -79,20 +64,16 @@ public class EditService extends AppCompatActivity {
         email = findViewById(R.id.editText3);
         phone = findViewById(R.id.editText4);
         maxsize = findViewById(R.id.editText6);
-
-        TextView title = findViewById(R.id.textView6);
-        title.setText("Edytuj usługę");
         accommodationText = findViewById(R.id.textView16);
         maxsizeText = findViewById(R.id.textView15);
-
         accommodation = findViewById(R.id.checkBox);
-
-        ViewPager viewPager = findViewById(R.id.viewPager);
-
         subcategory = findViewById(R.id.spinner2);
         category = findViewById(R.id.spinner);
 
-        Button addPhoto = findViewById(R.id.addPhotoButton);
+        TextView title = findViewById(R.id.textView6);
+        title.setText("Edytuj usługę");
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
         Button addService = findViewById(R.id.button3);
         addService.setText("Edytuj usługę");
 
@@ -110,11 +91,23 @@ public class EditService extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         viewPager.setPageTransformer(true, new PageTransformer());
 
-        addPhoto.setOnClickListener(view -> openFileChooser());
 
+        initializeAddPhotoHandler();
         buildCategorySpinner();
         setValuesFromService(service);
+    }
 
+    private void initializeAddPhotoHandler() {
+        findViewById(R.id.addPhotoButton).setOnClickListener(view -> openFileChooser());
+    }
+
+    private void openFileChooser() {
+        final int PICK_IMAGE_REQUEST = 1;
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
     private void setValuesFromService(Service service) {
@@ -126,11 +119,6 @@ public class EditService extends AppCompatActivity {
         int categoryIndex = findPositionInCategorySpinner(categories, service.getCategory().getName());
         category.setSelection(categoryIndex);
 
-        if (service.getSubcategory() != null) {
-//            List<Subcategory> possibleSubcategories = categories.get(categoryIndex-1).getSubcategories();
-//            subcategory.setSelection(findPositionInSubcategorySpinner(possibleSubcategories, service.getSubcategory().getName()));
-            //getIndex(subcategory, service.getSubcategory().getName());
-        }
         if (service.getWeddingHallDetails() != null) {
             maxsize.setText(Integer.toString(service.getWeddingHallDetails().getMaxNumberOfGuests()));
             accommodation.setChecked(service.getWeddingHallDetails().isCanSleep());
@@ -138,17 +126,6 @@ public class EditService extends AppCompatActivity {
         if (service.getTransportDetails() != null) {
             maxsize.setText(Integer.toString(service.getTransportDetails().getMaxSittingPlaces()));
         }
-
-    }
-
-    private int getIndex(Spinner spinner, String myString) {
-        for (int i = 0; i < spinner.getCount(); i++) {
-            if (spinner.getItemAtPosition(i).toString().equals(myString)) {
-                return i;
-            }
-        }
-
-        return 0;
     }
 
     private int findPositionInCategorySpinner(List<Category> categories, String name) {
@@ -163,7 +140,6 @@ public class EditService extends AppCompatActivity {
     }
 
     private int findPositionInSubcategorySpinner(List<Subcategory> subcategories, String name) {
-        subcategories.indexOf(name);
         int i = 0;
         for (Subcategory c : subcategories) {
             i++;
@@ -237,7 +213,7 @@ public class EditService extends AppCompatActivity {
             if (!wrongData) {
                 Service createdService = addNewService(values, serviceDescription, serviceAccommodation);
                 ServerConnector<Service> serverConnector = new ServerConnector<>(ServiceName.services);
-                serverConnector.update(index, createdService);
+                serverConnector.update(service.getId(), createdService);
                 return "Usługa została pomyślnie zmodyfikowana";
             } else {
                 return result.toString();
@@ -333,47 +309,39 @@ public class EditService extends AppCompatActivity {
         }
     }
 
-    private void openFileChooser() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
-    }
-
     private void buildCategorySpinner() {
+        ArrayList<String> helper = new ArrayList<String>() {{ add("Wybierz..."); }};
+        helper.addAll(categories.stream().map(Category::getName).collect(Collectors.toList()));
 
-        final ArrayList<String> helper = new ArrayList<String>();
-        helper.add("Wybierz...");
-        for (int i = 0; i < categories.size(); i++) {
-            helper.add(categories.get(i).getName());
-        }
-        ArrayAdapter<String> CategoriesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper);
-        category.setAdapter(CategoriesAdapter);
+        category.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper));
         category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // First item will be gray
                 if (position == 0) {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.darker_gray));
-                    buildSubcategorySpinner();
+                    setColorToTextView(view, android.R.color.darker_gray);
+                    buildSubcategorySpinner("Wybierz...");
                     subcategory.setEnabled(false);
-                } else {
-
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.black));
-                    if (categories.get(position - 1).getSubcategories() != null) {
-                        subcategory.setEnabled(true);
-                        buildSubcategorySpinner(categories.get(position - 1).getSubcategories());
-                        if (service.getSubcategory() != null) {
-                            List<Subcategory> possibleSubcategories = categories.get(position - 1).getSubcategories();
-                            subcategory.setSelection(findPositionInSubcategorySpinner(possibleSubcategories, service.getSubcategory().getName()));
-                        }
-                    } else {
-                        buildEmptySubcategorySpinner();
-                        subcategory.setEnabled(false);
-                    }
-
-
+                    return;
                 }
+
+                setColorToTextView(view, android.R.color.black);
+
+                List<Subcategory> subcategories = categories.get(position - 1).getSubcategories();
+                boolean doesCategoryContainSubcategories = subcategories != null;
+                if (doesCategoryContainSubcategories) {
+                    subcategory.setEnabled(true);
+                    buildSubcategorySpinner("Wybierz...", subcategories);
+
+                    if (service.getSubcategory() != null) {
+                        subcategory.setSelection(findPositionInSubcategorySpinner(subcategories, service.getSubcategory().getName()));
+                    }
+                } else {
+                    buildSubcategorySpinner("Brak podkategorii");
+                    subcategory.setEnabled(false);
+                }
+
                 setVisibilityInCaseOfCategory(helper.get(position));
             }
 
@@ -383,24 +351,19 @@ public class EditService extends AppCompatActivity {
         });
     }
 
+    private void buildSubcategorySpinner(String helperText) {
+        buildSubcategorySpinner(helperText, new ArrayList<>());
+    }
 
-    private void buildSubcategorySpinner(List<Subcategory> subcategories) {
-        ArrayList<String> helper = new ArrayList<String>();
-        helper.add("Wybierz...");
-        for (int i = 0; i < subcategories.size(); i++) {
-            helper.add(subcategories.get(i).getName());
-        }
-        ArrayAdapter<String> subcategoriesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper);
-        subcategory.setAdapter(subcategoriesAdapter);
+    private void buildSubcategorySpinner(String helperText, List<Subcategory> subcategories) {
+        ArrayList<String> helper = new ArrayList<String>() {{ add(helperText); }};
+        helper.addAll(subcategories.stream().map(Subcategory::getName).collect(Collectors.toList()));
+
+        subcategory.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper));
         subcategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // First item will be gray
-                if (position == 0) {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.darker_gray));
-                } else {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.black));
-                }
+                setColorToTextView(view, position == 0 ? android.R.color.darker_gray : android.R.color.black);
             }
 
             @Override
@@ -409,75 +372,7 @@ public class EditService extends AppCompatActivity {
         });
     }
 
-    private void buildSubcategorySpinnerAndSetSelection(List<Subcategory> subcategories, int position) {
-        ArrayList<String> helper = new ArrayList<String>();
-        helper.add("Wybierz...");
-        for (int i = 0; i < subcategories.size(); i++) {
-            helper.add(subcategories.get(i).getName());
-        }
-        ArrayAdapter<String> subcategoriesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper);
-        subcategory.setAdapter(subcategoriesAdapter);
-        subcategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // First item will be gray
-                if (position == 0) {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.darker_gray));
-                } else {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.black));
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-        subcategory.setSelection(position);
+    private void setColorToTextView(View view, int color) {
+        ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, color));
     }
-
-    private void buildEmptySubcategorySpinner() {
-        ArrayList<String> helper = new ArrayList<String>();
-        helper.add("Brak podkategorii");
-        ArrayAdapter<String> subcategoriesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper);
-        subcategory.setAdapter(subcategoriesAdapter);
-        subcategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // First item will be gray
-                if (position == 0) {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.darker_gray));
-                } else {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.black));
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-
-    private void buildSubcategorySpinner() {
-        ArrayList<String> helper = new ArrayList<String>();
-        helper.add("Wybierz...");
-        ArrayAdapter<String> subcategoriesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, helper);
-        subcategory.setAdapter(subcategoriesAdapter);
-        subcategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // First item will be gray
-                if (position == 0) {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.darker_gray));
-                } else {
-                    ((TextView) view).setTextColor(ContextCompat.getColor(EditService.this, android.R.color.black));
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-
-
 }
